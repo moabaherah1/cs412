5966b0bdc41320740cfe211b9f86c077c0fe0232
moabaherah1 <abaherah@bu.edu>
2025-02-07 01:06:29 -0500

    problems with admin ... removed symlink temporarily

--- cs412/settings.py
+++ cs412/settings.py
@@ -131,9 +131,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL= "media/"  # note: no leading slash!
