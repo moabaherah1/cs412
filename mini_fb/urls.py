ccb28bb26e3ab239e992e1dc61dde02a858a7d42
moabaherah1 <abaherah@bu.edu>
2025-02-21 00:20:12 -0500

    added comment somewhere

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -5,9 +5,9 @@
 from django.urls import path
 from .views import ShowAllProfilesView, ShowProfilePageView
 
-
+'''our url patterns so we can link to the page we want to show'''
 urlpatterns = [
-
+    
     path('', ShowAllProfilesView.as_view(), name="show_all_profiles"),
     path('profile/<int:pk>', ShowProfilePageView.as_view(), name='show_profile'),
 
